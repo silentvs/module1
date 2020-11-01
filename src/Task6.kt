3cc2d399c03c377a47f13d52e8f4339ab3b20606
Siyamend Vladimir <Siyamend.V.G@sberbank.ru>
2020-11-01 17:53:21 +0300

    Задание 6: Сравнение

---
+++ src/Task6.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+
+    try {
+        println("Введите первое число - a:")
+        var aInput: String? = readLine()
+        var aFloat: Float = aInput.toString().replace(',', '.').toFloat()
+        println("Введите второе число - b:")
+        var bInput: String? = readLine()
+        var bFloat: Float = bInput.toString().replace(',', '.').toFloat()
+        var higher: Boolean = aFloat > bFloat
+        println("Число a больше b: $higher")
+    } catch (e: NumberFormatException) {
+        println("Введено не число")
+    }
+}
