e2b85cab6fdc85c86b8bda7ae8626ff4cb7d5e3c
Siyamend Vladimir <Siyamend.V.G@sberbank.ru>
2020-11-01 03:54:15 +0300

    Задание 3: Преобразование

---
+++ src/Task3.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+
+    var five: Char = '5'
+    var fiveStr = five.toString()
+    var fiveByte = five.toByte()
+    var fiveShort = five.toShort()
+    var fiveInt = five.toInt()
+    var fiveDouble = five.toDouble()
+    var fiveFloat = five.toFloat()
+    var fiveLong = five.toLong()
+
+}
